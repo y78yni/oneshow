af7aad3ed1e7a81db3aab4ce20caa0c8287a710d
home <402663344@qq.com>
2019-07-01 19:08:59 +0800

    no message

---
+++ src/main/java/com/jun/oneshow/OneshowApplication.java
@@ -0,0 +1,13 @@
+package com.jun.oneshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneshowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneshowApplication.class, args);
+	}
+
+}
