f63482b80dfb6a1fd59f45fb3b8163353e8ac255
home <402663344@qq.com>
2019-07-02 22:07:42 +0800

    no message

---
+++ src/main/java/com/jun/oneshow/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.jun.oneshow.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	@RequestMapping("test")
+    public String test() {
+        return "Hello springboot!";
+    }
+}
--- src/test/java/com/jun/oneshow/OneshowApplicationTests.java
+++ src/test/java/com/jun/oneshow/OneshowApplicationTests.java
@@ -1,16 +1,37 @@
-package com.jun.oneshow;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class OneshowApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+//package com.jun.oneshow;
+//
+//import com.jun.oneshow.entity.User;
+//import com.jun.oneshow.mapper.UserMapper;
+//import org.junit.Assert;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import java.util.List;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class OneshowApplicationTests {
+//
+//	@Autowired
+//	private UserMapper userMapper;
+//
+//	@Test
+//	public void testSelect() {
+//		System.out.println(("----- selectAll method test ------"));
+//		List<User> userList = userMapper.selectList(null);
+//		Assert.assertEquals(5, userList.size());
+//		userList.forEach(System.out::println);
+//	}
+//	//这个是引入xml的自定义方法，xml文件不是必须
+//	@Test
+//	public void testSelect2() {
+//		System.out.println(("----- select222All method test ------"));
+//		List<User> userList = userMapper.list();
+//		Assert.assertEquals(5, userList.size());
+//		userList.forEach(System.out::println);
+//	}
+//
+//}
